5a6b123630cb1b506c13e86378a77b6435688cb2
István Palásthy <istvanpalasthy@yahoo.com>
2020-05-29 10:26:44 +0200

    menjfel

---
+++ Domain/Tweet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tweetcool
+{
+    public class Tweet
+    {
+        public string UserName { get; set; }
+        public string Comment { get; set; }
+        public DateTime TweetingTime { get; set; }
+
+
+
+        public Tweet(string username, string comment)
+        {
+            UserName = username;
+            Comment = comment;
+            TweetingTime = DateTime.Now;
+        }
+    }
+}
